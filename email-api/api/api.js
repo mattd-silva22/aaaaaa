4b928cab469409288439d15c3ba2d332be91457f
Matheus Barretto <78603546+mattd-silva22@users.noreply.github.com>
2021-10-26 12:06:03 -0300

    Add files via upload

--- email-api/api/api.js
+++ email-api/api/api.js
@@ -1,6 +1,27 @@
 const express = require("express");
 const app = express();
 
+
+const nodemailer = require("nodemailer");
+
+let transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: "matheus1822silva@gmail.com",
+        pass: "teste@19141918"
+    }
+})
+
+let mailOptions = {
+    from: " 'Matheus'  <matheus1822silva@gmail.com>",
+    to: "matheus.barretto2001@gmail.com",
+    subject: " mentiras",
+    text:"ola mundo !",
+    html: "<h1>Teste h1</h1>"
+
+};
+
+
 const port = 3000;
 
 app.use(express.static("./public"));
@@ -11,6 +32,23 @@ app.get("/teste", (req , res)=> {
     res.statusCode = 200;
     res.setHeader('Content-Type' , 'text/html')
     res.end("<h1> ola mundo 235</h1>")
+});
+
+app.get("/email", (req , res)=> {
+    transporter.sendMail(mailOptions ,(err , data)=>{
+        if(err) {
+            console.log("deu ruim", err)
+            res.statusCode = 300;
+            res.setHeader('Content-Type' , 'text/html')
+            res.end("<h1>ruim</h1>")
+        } else {
+            console.log("mentira enviada")
+            res.statusCode = 200;
+            res.setHeader('Content-Type' , 'text/html')
+            res.end("<h1>bom</h1>")
+        }
+    })
+    
 });
 
 
