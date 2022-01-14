ccc34991fd1b7a0476ffc28caff52a068b536983
Matheus Barretto <78603546+mattd-silva22@users.noreply.github.com>
2022-01-14 14:49:20 -0300

    Delete js directory

--- js/index.js
+++
@@ -1,8 +0,0 @@
-hamBtn = document.querySelector("#ham-btn");
-navResp = document.querySelector("#nav-resp")
-
-
-hamBtn.addEventListener("click" , ()=> {
-    hamBtn.classList.toggle("active")
-    navResp.classList.toggle("active")
-})
